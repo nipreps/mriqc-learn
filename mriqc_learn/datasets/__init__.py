c9bf3ed6c9c2d3751785643d8477bd3622c63d67
Céline Provins <celine.provins@unil.ch>
2024-04-24 14:30:16 +0200

    fix: make load_data more flexible by enable pd.dataframe as input

--- mriqc_learn/datasets/__init__.py
+++ mriqc_learn/datasets/__init__.py
@@ -65,8 +65,8 @@ def load_data(
 
     Parameters
     ----------
-    path : :obj:`os.pathlike`
-        Whether to indicate a custom path were data are written in a TSV file.
+    path : :obj:`os.pathlike` or :obj:`pd.DataFrame`
+        Either a custom path where data are written in a TSV file, or a pandas DataFrame containing the data directly.
     split_strategy : ``None`` or :obj:`str`
         How the data must be split into train and test subsets.
         Possible values are: ``"random"`` (default), ``"site"``, or ``None``/``"none"``.
@@ -88,10 +88,14 @@ def load_data(
     if site is not None:
         split_strategy = "site"
 
-    if path is None:
-        path = Path(pkgrf("mriqc_learn.datasets", "abide.tsv"))
+    if isinstance(path, pd.DataFrame):
+        dataframe = path
+    else:
+        if path is None:
+            path = Path(pkgrf("mriqc_learn.datasets", "abide.tsv"))
 
-    dataframe = pd.read_csv(path, index_col=None, delimiter=r"\s+")
+        dataframe = pd.read_csv(path, index_col=None, delimiter=r"\s+")
+
     xy_index = dataframe.columns.tolist().index(first_iqm)
 
     if split_strategy is None or split_strategy.lower() == "none":
